c1d5771534394d3eee5f4d95319820fcf0794840
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-07 16:47:10 -0700

    added 2x pages

--- dojo_survey/main/urls.py
+++ dojo_survey/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('results', views.results),
+    path('results/<int:num_of_results>', views.number_of_results),
 ]
--- dojo_survey/main/views.py
+++ dojo_survey/main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Wooo I got it!")
+
+def results(request):
+    return HttpResponse("This is the response page!")
+
+def number_of_results(request, num_of_results):
+    return HttpResponse(f"I have {num_of_results} as a result")
