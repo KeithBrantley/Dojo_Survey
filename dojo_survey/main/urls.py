06a16d914300cb3736f4c00fbca9b9668aadec3f
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-07 17:41:49 -0700

    Working on HTML Docs

--- dojo_survey/main/urls.py
+++ dojo_survey/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('results', views.results),
-    path('results/<int:num_of_results>', views.number_of_results),
+    path('results/go_back', views.direct_to_index),
 ]
--- dojo_survey/main/views.py
+++ dojo_survey/main/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Wooo I got it!")
+    return render(request, 'index.html')
 
 def results(request):
-    return HttpResponse("This is the response page!")
+     return render(request, 'results.html')
 
-def number_of_results(request, num_of_results):
-    return HttpResponse(f"I have {num_of_results} as a result")
+def direct_to_index(request):
+    print("I'm going home!")
+    return redirect('/')
+
