4c3fa70772d4eb01b7efeee1d3a18bf8659cbcc0
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-07 21:08:29 -0700

    Need to implement redircts

--- dojo_survey/main/views.py
+++ dojo_survey/main/views.py
@@ -5,9 +5,16 @@ def index(request):
     return render(request, 'index.html')
 
 def results(request):
-     return render(request, 'results.html')
+    # print(request.POST)
+    # print(request.POST['name'])
+    context={
+        'name': request.POST["name"],
+        'dojo_location' : request.POST["dojo_location"],
+        'favorite_language': request.POST["favorite_language"],
+        'comment': request.POST["comment"]
+    }
+    return render(request, 'results.html', context)
 
 def direct_to_index(request):
-    print("I'm going home!")
     return redirect('/')
 
